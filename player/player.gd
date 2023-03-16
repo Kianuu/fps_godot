d4f6a71533b47126503237ef756f98fca226ee92
Kianuu <krezazadeh@hotmail.com>
2023-03-16 18:40:50 -0400

    Added player camera, mouse capture for player camera and fixed script for Godot 4.0

---
+++ player/player.gd
@@ -0,0 +1,29 @@
+extends CharacterBody3D
+
+@export var mouse_sens = 0.5
+
+@onready var camera = $Camera3D
+
+func _ready():
+	Input.set_mouse_mode(2)
+	
+
+func _process(delta):
+	if Input.is_action_just_pressed("exit"):
+		get_tree().quit()
+	
+	var move_vec = Vector3()
+	if Input.is_action_pressed("move_forwards"):
+		move_vec += Vector3.FORWARD
+	if Input.is_action_pressed("move_backwards"):
+		move_vec += Vector3.BACK
+	if Input.is_action_pressed("move_left"):
+		move_vec += Vector3.LEFT
+	if Input.is_action_pressed("move_right"):
+		move_vec += Vector3.RIGHT
+	
+func _input(event):
+	if event is InputEventMouseMotion:
+		rotation_degrees.y -= mouse_sens * event.relative.x
+		camera.rotation_degrees.x -= mouse_sens * event.relative.y
+		camera.rotation_degrees.x = clamp(camera.rotation_degrees.x, -90, 90)
